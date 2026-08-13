fe0f2242f83c444b022099f0a9199ce15870ca37
Rajrup Mal <rajrup0912@gmail.com>
2026-08-14 03:40:50 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int climbStairs(int n) {
+        int ways[] = new int[n + 1];
+        Arrays.fill(ways, -1);
+
+        return countWays(n, ways);
+    }
+    public int countWays(int n, int ways[]) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+
+        ways[n] = countWays(n - 1, ways) + countWays(n - 2, ways);
+
+        return ways[n];
+    }
+}
